cbc5a7db9ca0e64c97490ec19633e4c688255a7e
Roland Zerek <rolandz@poczta.fm>
2019-04-15 08:35:51 +0200

    Added list box to start view

--- src/Messenger.UI/Views/StartView.Designer.cs
+++ src/Messenger.UI/Views/StartView.Designer.cs
@@ -28,23 +28,23 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.button1 = new System.Windows.Forms.Button();
+			this.lbFavorites = new System.Windows.Forms.ListBox();
 			this.SuspendLayout();
 			// 
-			// button1
+			// lbFavorites
 			// 
-			this.button1.Location = new System.Drawing.Point(41, 37);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(75, 23);
-			this.button1.TabIndex = 0;
-			this.button1.Text = "button1";
-			this.button1.UseVisualStyleBackColor = true;
+			this.lbFavorites.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.lbFavorites.FormattingEnabled = true;
+			this.lbFavorites.Location = new System.Drawing.Point(0, 0);
+			this.lbFavorites.Name = "lbFavorites";
+			this.lbFavorites.Size = new System.Drawing.Size(150, 150);
+			this.lbFavorites.TabIndex = 0;
 			// 
 			// StartView
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Controls.Add(this.button1);
+			this.Controls.Add(this.lbFavorites);
 			this.Name = "StartView";
 			this.ResumeLayout(false);
 
@@ -52,6 +52,6 @@
 
 		#endregion
 
-		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.ListBox lbFavorites;
 	}
 }
